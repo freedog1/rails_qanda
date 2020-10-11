d2a3de80a608f8b0ba1bac97602214fbac426b8d
freedog1 <koki375656@gmail.com>
2020-10-11 03:12:38 +0000

    answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_224157) do
+ActiveRecord::Schema.define(version: 2020_10_11_030707) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "questtion_id"
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["questtion_id"], name: "index_answers_on_questtion_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "name"
